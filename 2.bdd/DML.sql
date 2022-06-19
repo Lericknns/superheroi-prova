8b3d1350a5df1a5bdd43dff9747eafdbb36dd1c5
Leandro Nunes <gerakleandro@gmail.com>
2022-06-18 21:32:55 -0300

    começo

---
+++ 2.bdd/DML.sql
@@ -0,0 +1,8 @@
+insert into tb_super_hero(nm_super_hero,ds_super_poder,bt_voa)
+     value('saitama','um soco',false);
+     
+            update tb_super_hero
+			   set nm_super_hero = 'saitama',
+                   ds_super_poder ='um soco',
+                   bt_voa = false
+                   
